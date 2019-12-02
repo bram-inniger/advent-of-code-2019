42c52e0b2a70d5090c3169ea09b0bb3fc3678921
Bram Inniger <bram.inniger@transferwise.com>
2019-12-02 09:18:16 +0100

    Clean extra dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,6 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
-    implementation(group = "io.vavr", name = "vavr-kotlin", version = "0.10.2")
     testImplementation(kotlin("test-junit"))
 }
+

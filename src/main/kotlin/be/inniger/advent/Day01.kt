bb138dac9105889047989373bd7bc896d8d829c0
Bram Inniger <bram.inniger@transferwise.com>
2019-12-02 09:44:21 +0100

    D01P01 - Shortened

--- src/main/kotlin/be/inniger/advent/Day01.kt
+++ src/main/kotlin/be/inniger/advent/Day01.kt
@@ -4,11 +4,5 @@ import kotlin.math.floor
 
 class Day01 {
 
-    fun solveFirst(masses: List<Int>) =
-        masses.map { it.toDouble() }
-            .map { it / 3 }
-            .map { floor(it) }
-            .map { it.toInt() }
-            .map { it - 2 }
-            .sum()
+    fun solveFirst(masses: List<Int>) = masses.map { floor(it.toDouble() / 3).toInt() - 2 }.sum()
 }

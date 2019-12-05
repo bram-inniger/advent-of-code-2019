b92bb682ed31c3eb858195c501f122f833297470
Bram Inniger <bram.inniger@transferwise.com>
2019-12-05 18:44:55 +0000

    D05P02 - Solved

--- src/main/kotlin/be/inniger/advent/Day05.kt
+++ src/main/kotlin/be/inniger/advent/Day05.kt
@@ -5,33 +5,44 @@ import kotlin.math.pow
 class Day05 {
 
     companion object {
-        private const val TEST_ID = 1
+        private const val CONDITIONER_ID = 1
+        private const val RADIATOR_ID = 5
     }
 
-    fun solveFirst(program: IntArray, input: Int = TEST_ID) =
+    fun solveFirst(program: IntArray, input: Int = CONDITIONER_ID) =
         runProgram(program, input)
 
-    private fun runProgram(originalProgram: IntArray, input: Int): String {
+    fun solveSecond(program: IntArray, input: Int = RADIATOR_ID) =
+        runProgram(program, input)
+
+    private fun runProgram(originalProgram: IntArray, input: Int): Int {
         val program = originalProgram.copyOf()
-        val diagnostics = StringBuilder()
         var pointer = 0
+        var diagnostics = 0
 
         while (true) {
             val instruction = program[pointer]
             val opcode = Opcode.parse(instruction % 100)
+            val arg = partialApply(::readArg, instruction, program, pointer)
 
             when (opcode) {
-                Opcode.ADD -> program[program[pointer + 3]] =
-                    readArg(instruction, program, pointer, 1) + readArg(instruction, program, pointer, 2)
-                Opcode.MULTIPLY -> program[program[pointer + 3]] =
-                    readArg(instruction, program, pointer, 1) * readArg(instruction, program, pointer, 2)
+                Opcode.ADD -> program[program[pointer + 3]] = arg(1) + arg(2)
+                Opcode.MULTIPLY -> program[program[pointer + 3]] = arg(1) * arg(2)
                 Opcode.INPUT -> program[program[pointer + 1]] = input
-                Opcode.OUTPUT -> diagnostics.append(readArg(instruction, program, pointer, 1))
-                Opcode.HALT -> return diagnostics.toString()
+                Opcode.OUTPUT -> diagnostics = diagnostics * 10 + arg(1)
+                Opcode.JT -> if (arg(1) != 0) pointer = arg(2) - opcode.instructionLength
+                Opcode.JF -> if (arg(1) == 0) pointer = arg(2) - opcode.instructionLength
+                Opcode.LT -> program[program[pointer + 3]] = if (arg(1) < arg(2)) 1 else 0
+                Opcode.EQ -> program[program[pointer + 3]] = if (arg(1) == arg(2)) 1 else 0
+                Opcode.HALT -> return diagnostics
             }
 
             pointer += opcode.instructionLength
         }
+    }
+
+    private fun <A, B, C, D, E> partialApply(f: (A, B, C, D) -> E, a: A, b: B, c: C): (D) -> E {
+        return { d: D -> f(a, b, c, d) }
     }
 
     private fun readArg(instruction: Int, program: IntArray, pointer: Int, argIndex: Int) =
@@ -39,14 +50,20 @@ class Day05 {
         else program[pointer + argIndex]
 
     private fun computeMode(instruction: Int, argIndex: Int) =
-        if (instruction / (10 * 10.0.pow(argIndex.toDouble()).toInt()) % 10 == 0) Mode.POSITION
+        if (instruction / (10 * 10.pow(argIndex)) % 10 == 0) Mode.POSITION
         else Mode.IMMEDIATE
+
+    private fun Int.pow(power: Int) = this.toDouble().pow(power.toDouble()).toInt()
 
     private enum class Opcode(val instructionLength: Int) {
         ADD(4),
         MULTIPLY(4),
         INPUT(2),
         OUTPUT(2),
+        JT(3),
+        JF(3),
+        LT(4),
+        EQ(4),
         HALT(1);
 
         companion object {
@@ -55,6 +72,10 @@ class Day05 {
                 2 -> MULTIPLY
                 3 -> INPUT
                 4 -> OUTPUT
+                5 -> JT
+                6 -> JF
+                7 -> LT
+                8 -> EQ
                 99 -> HALT
                 else -> error("Cannot parse opcode: $opcode")
             }
--- src/test/kotlin/be/inniger/advent/Day05Test.kt
+++ src/test/kotlin/be/inniger/advent/Day05Test.kt
@@ -11,15 +11,75 @@ class Day05Test {
 
     @Test
     fun validateFirstSampleInputs() {
-        assertEquals("42", problem.solveFirst(intArrayOf(3, 0, 4, 0, 99), 42))
+        assertEquals(42, problem.solveFirst(intArrayOf(3, 0, 4, 0, 99), 42))
 
-        assertEquals("", problem.solveFirst(intArrayOf(1002, 4, 3, 4, 33)))
+        assertEquals(0, problem.solveFirst(intArrayOf(1002, 4, 3, 4, 33)))
 
-        assertEquals("", problem.solveFirst(intArrayOf(1101, 100, -1, 4, 0)))
+        assertEquals(0, problem.solveFirst(intArrayOf(1101, 100, -1, 4, 0)))
     }
 
     @Test
     fun validateFirstSolution() {
-        assertEquals(11049715, problem.solveFirst(input).toInt())
+        assertEquals(11049715, problem.solveFirst(input))
+    }
+
+    @Test
+    fun validateSecondSampleInputs() {
+        assertEquals(0, problem.solveFirst(intArrayOf(3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8), 7))
+        assertEquals(1, problem.solveFirst(intArrayOf(3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8), 8))
+        assertEquals(0, problem.solveFirst(intArrayOf(3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8), 9))
+
+        assertEquals(1, problem.solveFirst(intArrayOf(3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8), 7))
+        assertEquals(0, problem.solveFirst(intArrayOf(3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8), 8))
+        assertEquals(0, problem.solveFirst(intArrayOf(3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8), 9))
+
+        assertEquals(0, problem.solveFirst(intArrayOf(3, 3, 1108, -1, 8, 3, 4, 3, 99), 7))
+        assertEquals(1, problem.solveFirst(intArrayOf(3, 3, 1108, -1, 8, 3, 4, 3, 99), 8))
+        assertEquals(0, problem.solveFirst(intArrayOf(3, 3, 1108, -1, 8, 3, 4, 3, 99), 9))
+
+        assertEquals(1, problem.solveFirst(intArrayOf(3, 3, 1107, -1, 8, 3, 4, 3, 99), 7))
+        assertEquals(0, problem.solveFirst(intArrayOf(3, 3, 1107, -1, 8, 3, 4, 3, 99), 8))
+        assertEquals(0, problem.solveFirst(intArrayOf(3, 3, 1107, -1, 8, 3, 4, 3, 99), 9))
+
+        assertEquals(1, problem.solveFirst(intArrayOf(3, 12, 6, 12, 15, 1, 13, 14, 13, 4, 13, 99, -1, 0, 1, 9), -1))
+        assertEquals(0, problem.solveFirst(intArrayOf(3, 12, 6, 12, 15, 1, 13, 14, 13, 4, 13, 99, -1, 0, 1, 9), 0))
+        assertEquals(1, problem.solveFirst(intArrayOf(3, 12, 6, 12, 15, 1, 13, 14, 13, 4, 13, 99, -1, 0, 1, 9), 1))
+
+        assertEquals(1, problem.solveFirst(intArrayOf(3, 3, 1105, -1, 9, 1101, 0, 0, 12, 4, 12, 99, 1), -1))
+        assertEquals(0, problem.solveFirst(intArrayOf(3, 3, 1105, -1, 9, 1101, 0, 0, 12, 4, 12, 99, 1), 0))
+        assertEquals(1, problem.solveFirst(intArrayOf(3, 3, 1105, -1, 9, 1101, 0, 0, 12, 4, 12, 99, 1), 1))
+
+        assertEquals(
+            999, problem.solveFirst(
+                intArrayOf(
+                    3, 21, 1008, 21, 8, 20, 1005, 20, 22, 107, 8, 21, 20, 1006, 20, 31,
+                    1106, 0, 36, 98, 0, 0, 1002, 21, 125, 20, 4, 20, 1105, 1, 46, 104,
+                    999, 1105, 1, 46, 1101, 1000, 1, 20, 4, 20, 1105, 1, 46, 98, 99
+                ), 7
+            )
+        )
+        assertEquals(
+            1000, problem.solveFirst(
+                intArrayOf(
+                    3, 21, 1008, 21, 8, 20, 1005, 20, 22, 107, 8, 21, 20, 1006, 20, 31,
+                    1106, 0, 36, 98, 0, 0, 1002, 21, 125, 20, 4, 20, 1105, 1, 46, 104,
+                    999, 1105, 1, 46, 1101, 1000, 1, 20, 4, 20, 1105, 1, 46, 98, 99
+                ), 8
+            )
+        )
+        assertEquals(
+            1001, problem.solveFirst(
+                intArrayOf(
+                    3, 21, 1008, 21, 8, 20, 1005, 20, 22, 107, 8, 21, 20, 1006, 20, 31,
+                    1106, 0, 36, 98, 0, 0, 1002, 21, 125, 20, 4, 20, 1105, 1, 46, 104,
+                    999, 1105, 1, 46, 1101, 1000, 1, 20, 4, 20, 1105, 1, 46, 98, 99
+                ), 9
+            )
+        )
+    }
+
+    @Test
+    fun validateSecondSolution() {
+        assertEquals(2140710, problem.solveSecond(input))
     }
 }

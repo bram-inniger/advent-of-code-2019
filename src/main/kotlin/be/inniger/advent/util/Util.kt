d7ac244a41fc582d94d0e3cf2cef5637cf48801f
Bram Inniger <bram.inniger@transferwise.com>
2019-12-07 20:39:40 +0000

    D07P01 - Further clean-up

--- src/main/kotlin/be/inniger/advent/util/Util.kt
+++ src/main/kotlin/be/inniger/advent/util/Util.kt
@@ -6,12 +6,12 @@ fun Int.pow(power: Int) = this.toDouble().pow(power.toDouble()).toInt()
 
 fun <T> List<T>.head() = first()
 fun <T> List<T>.tail() = drop(1)
-fun <T> List<T>.minusIndex(index: Int) = this[index] to subList(0, index) + subList(index + 1, size)
 
 fun <T> generatePermutations(elements: List<T>): List<List<T>> =
     if (elements.isEmpty()) listOf(emptyList())
     else elements.indices
         .flatMap { index ->
-            val (element, remainingElements) = elements.minusIndex(index)
+            val element = elements[index]
+            val remainingElements = elements.subList(0, index) + elements.subList(index + 1, elements.size)
             generatePermutations(remainingElements).map { it + element }
         }

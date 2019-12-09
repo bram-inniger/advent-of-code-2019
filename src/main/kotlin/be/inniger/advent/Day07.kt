e7104e1500c8343d50231f426c14af021a2bd9bd
Bram Inniger <bram.inniger@transferwise.com>
2019-12-08 23:35:39 -0500

    D07P02 - Solved

--- src/main/kotlin/be/inniger/advent/Day07.kt
+++ src/main/kotlin/be/inniger/advent/Day07.kt
@@ -9,19 +9,38 @@ class Day07 {
 
     companion object {
         private const val FIRST_INPUT = 0
-        private const val FIRST_AMP_PHASE = 0
-        private const val LAST_AMP_PHASE = 4
     }
 
-    fun solveFirst(program: List<Int>) = generatePermutations((FIRST_AMP_PHASE..LAST_AMP_PHASE).toList())
-        .map { runAmplifiers(program, it) }
+    fun solveFirst(program: List<Int>) = generatePermutations((0..4).toList())
+        .map { createComputers(program, it) }
+        .map { runAmplifiers(it) }
         .max()!!
 
-    private tailrec fun runAmplifiers(originalProgram: List<Int>, phases: List<Int>, output: Int = FIRST_INPUT): Int =
-        if (phases.isEmpty()) output
-        else runAmplifiers(
-            originalProgram,
-            phases.tail(),
-            IntComputer(originalProgram, phases.head()).runProgram(output).output
-        )
+    fun solveSecond(program: List<Int>) = generatePermutations((5..9).toList())
+        .map { createComputers(program, it) }
+        .map { runAmplifiersWithFeedback(it) }
+        .max()!!
+
+    private fun createComputers(program: List<Int>, phases: List<Int>) =
+        phases.map { phase -> IntComputer(program, phase) }
+
+    private tailrec fun runAmplifiers(computers: List<IntComputer>, output: Int = FIRST_INPUT): Int =
+        if (computers.isEmpty()) output
+        else runAmplifiers(computers.tail(), computers.head().runProgram(output).output)
+
+    private tailrec fun runAmplifiersWithFeedback(
+        computers: List<IntComputer>,
+        output: Int = FIRST_INPUT,
+        nrHalts: Int = 0,
+        currentComputer: Int = 0
+    ): Int =
+        if (nrHalts >= computers.size) output
+        else {
+            val state = computers[currentComputer].runProgram(output)
+
+            val updatedNrHalts = nrHalts + if (state.halted) 1 else 0
+            val nextComputer = (currentComputer + 1) % computers.size
+
+            runAmplifiersWithFeedback(computers, state.output, updatedNrHalts, nextComputer)
+        }
 }
--- src/test/kotlin/be/inniger/advent/Day07Test.kt
+++ src/test/kotlin/be/inniger/advent/Day07Test.kt
@@ -37,4 +37,31 @@ class Day07Test {
     fun validateFirstSolution() {
         assertEquals(262086, problem.solveFirst(input))
     }
+
+    @Test
+    fun validateSecondSampleInputs() {
+        assertEquals(
+            139629729, problem.solveSecond(
+                listOf(
+                    3, 26, 1001, 26, -4, 26, 3, 27, 1002, 27, 2, 27, 1, 27, 26,
+                    27, 4, 27, 1001, 28, -1, 28, 1005, 28, 6, 99, 0, 0, 5
+                )
+            )
+        )
+
+        assertEquals(
+            18216, problem.solveSecond(
+                listOf(
+                    3, 52, 1001, 52, -5, 52, 3, 53, 1, 52, 56, 54, 1007, 54, 5, 55, 1005, 55, 26, 1001, 54,
+                    -5, 54, 1105, 1, 12, 1, 53, 54, 53, 1008, 54, 0, 55, 1001, 55, 1, 55, 2, 53, 55, 53, 4,
+                    53, 1001, 56, -1, 56, 1005, 56, 6, 99, 0, 0, 0, 0, 10
+                )
+            )
+        )
+    }
+
+    @Test
+    fun validateSecondSolution() {
+        assertEquals(5371621, problem.solveSecond(input))
+    }
 }
